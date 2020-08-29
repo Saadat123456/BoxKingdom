0628a97af63b848350c0ba8937d20a69dab56259
Saadat123456 <35307862+Saadat123456@users.noreply.github.com>
2020-08-29 19:35:16 +0500

    Second Commit

---
+++ Box Kingdom/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    private List<string> linesFormed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        linesFormed = new List<string>();
+    }
+
+    public void addToList(string item)
+    {
+        linesFormed.Add(item);
+    }
+
+    /*
+    public void getNumberOfLines(int startingNumber, int numberOfColumns, int mouseOverObjectNumber)
+    {
+        if ((startingNumber + numberOfColumns) == mouseOverObjectNumber || (startingNumber - numberOfColumns) == mouseOverObjectNumber)
+        {
+            int numberOfLines = 0;
+            string toCheck = (startingNumber - 1).ToString() + startingNumber.ToString();
+            if (linesFormed.Contains(toCheck))
+                numberOfLines++;
+            toCheck = (startingNumber - 1).ToString() + (startingNumber - 1 + numberOfColumns).ToString();
+            if (linesFormed.Contains(toCheck))
+                numberOfLines++;
+            toCheck = (startingNumber - 1 + numberOfColumns).ToString() + (startingNumber + numberOfColumns).ToString();
+            if (linesFormed.Contains(toCheck))
+                numberOfLines++;
+            if (numberOfLines == 3)
+                Debug.Log("Daba Ban Gia Left");
+
+            numberOfLines = 0;
+            toCheck = startingNumber.ToString() + (startingNumber + 1).ToString();
+            if (linesFormed.Contains(toCheck))
+                numberOfLines++;
+            toCheck = (startingNumber + 1).ToString() + (startingNumber + 1 + numberOfColumns).ToString();
+            if (linesFormed.Contains(toCheck))
+                numberOfLines++;
+            toCheck = (startingNumber + numberOfColumns).ToString() + (startingNumber + +numberOfColumns + 1).ToString();
+            if (linesFormed.Contains(toCheck))
+                numberOfLines++;
+
+
+            if (numberOfLines == 3)
+                Debug.Log("Daba Ban Gia Right");
+        }
+        else
+        {
+            if ((startingNumber + 1) == mouseOverObjectNumber || (startingNumber - 1) == mouseOverObjectNumber)
+            {
+                int numberOfLines = 0;
+                string toCheck = (startingNumber - numberOfColumns).ToString() + (startingNumber).ToString();
+                if (linesFormed.Contains(toCheck))
+                    numberOfLines++;
+                toCheck = (startingNumber - numberOfColumns).ToString() + (startingNumber + 1 - numberOfColumns).ToString();
+                if (linesFormed.Contains(toCheck))
+                    numberOfLines++;
+                toCheck = (startingNumber + 1 - numberOfColumns).ToString() + (startingNumber + 1).ToString();
+                if (linesFormed.Contains(toCheck))
+                    numberOfLines++;
+
+                if (numberOfLines == 3)
+                    Debug.Log("Daba Ban Gia Ooper");
+
+                numberOfLines = 0;
+                toCheck = (startingNumber).ToString() + (startingNumber + numberOfColumns).ToString();
+                if (linesFormed.Contains(toCheck))
+                    numberOfLines++;
+                toCheck = (startingNumber + numberOfColumns).ToString() + (startingNumber + 1 + numberOfColumns).ToString();
+                if (linesFormed.Contains(toCheck))
+                    numberOfLines++;
+                toCheck = (startingNumber + 1).ToString() + (startingNumber + numberOfColumns + 1).ToString();
+                if (linesFormed.Contains(toCheck))
+                    numberOfLines++;
+
+                if (numberOfLines == 3)
+                    Debug.Log("Daba Ban Gia Neechy");
+            }
+        }
+    }
+    */
+
+    public void getNumberOfLines(int startingNumber, int numberOfColumns, int mouseOverObjectNumber)
+    {
+        int x, y;
+        if (startingNumber < mouseOverObjectNumber)
+        {
+            x = startingNumber;
+            y = mouseOverObjectNumber;
+        }
+        else
+        {
+            y = startingNumber;
+            x = mouseOverObjectNumber;
+        }
+        int n = numberOfColumns;
+        int numberOfLines = 0;
+        if ((startingNumber + numberOfColumns) == mouseOverObjectNumber || (startingNumber - numberOfColumns) == mouseOverObjectNumber)
+        {
+            string querry = x.ToString() + (x + 1).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (y).ToString() + (y + 1).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (x+1).ToString() + (y + 1).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+
+            if(numberOfLines == 3)
+                Debug.Log("Right");
+            numberOfLines = 0;
+            querry = (x - 1).ToString() + (x).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (y-1).ToString() + (y).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (x-1).ToString() + (y-1).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            if(numberOfLines==3)
+                Debug.Log("Left");
+        }
+        else
+        {
+            string querry = x.ToString() + (x + n).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (x + n).ToString() + (y + n).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (y).ToString() + (y + n).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+
+            if(numberOfLines==3)
+                Debug.Log("DOWN");
+            numberOfLines = 0;
+            querry = (x-n).ToString() + (x).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (x - n).ToString() + (y - n).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+            querry = (y-n).ToString() + (y).ToString();
+            if (linesFormed.Contains(querry))
+                numberOfLines++;
+
+            if(numberOfLines==3)
+                Debug.Log("UP");
+        }
+    }
+}
--- Box Kingdom/Assets/Scripts/LinesController.cs
+++ Box Kingdom/Assets/Scripts/LinesController.cs
@@ -13,15 +13,31 @@ public class LinesController : MonoBehaviour
     private GameObject[] dots;
     private Boolean drawing;
     private Boolean drawingDone;
+    private GameObject mouseOverObject;
+    private GameObject startingObject;
+    private Material materialRight, materialNormal, materialWrong;
+    public int numberOfColumns;
+
+    GameObject gameController;
+    GameControllerScript gameControllerScript;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        numberOfColumns = 3;
         lineRend = GetComponent<LineRenderer>();
         dots = GameObject.FindGameObjectsWithTag("Dots");
         drawing = false;
         drawingDone = false;
+        mouseOverObject = null;
+
+        gameController = GameObject.Find("GameController");
+        gameControllerScript = gameController.GetComponent<GameControllerScript>();
+        
+        materialRight = Resources.Load("Material/Right", typeof(Material)) as Material;
+        materialNormal = Resources.Load("Material/Normal", typeof(Material)) as Material;
+        materialWrong = Resources.Load("Material/Wrong", typeof(Material)) as Material;
     }
 
     // Update is called once per frame
@@ -35,13 +51,15 @@ public class LinesController : MonoBehaviour
             {
                 if (hitInfo.collider.gameObject.CompareTag("Dots"))
                 {
-                    Debug.Log(hitInfo.collider.gameObject.transform.position);
+                    startingObject = hitInfo.collider.gameObject;
                     lineRend.positionCount = 2;
                     startMousePos = hitInfo.collider.gameObject.transform.position;
                     drawing = true;
                 }
             }
         }
+
+
 
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0))
         {
@@ -50,8 +68,46 @@ public class LinesController : MonoBehaviour
                 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 lineRend.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
                 lineRend.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));
+                RaycastHit hitInfo = new RaycastHit();
+                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+                if (hit)
+                {
+                    if (hitInfo.collider.gameObject.CompareTag("Dots"))
+                    {
+                        if (startingObject != hitInfo.collider.gameObject)
+                        {
+                            int startingNumber = int.Parse(startingObject.name);
+                            int mouseOverObjectNumber = int.Parse(hitInfo.collider.gameObject.name);
+                            
+                            if((mouseOverObjectNumber == startingNumber+1 && (mouseOverObjectNumber % numberOfColumns) != 1) || (mouseOverObjectNumber == startingNumber - 1 && (mouseOverObjectNumber % numberOfColumns) != 0) || mouseOverObjectNumber == startingNumber + numberOfColumns || mouseOverObjectNumber == startingNumber - numberOfColumns )
+                            {
+                                mouseOverObject = hitInfo.collider.gameObject;
+                                mouseOverObject.GetComponent<Renderer>().material = materialRight;
+                            }
+                            else
+                            {
+                                mouseOverObject = hitInfo.collider.gameObject;
+                                mouseOverObject.GetComponent<Renderer>().material = materialWrong;
+                            }
+                                
+                        }
+                    }
+                }
+                else
+                {
+                    if (mouseOverObject != null && mouseOverObject.GetComponent<Renderer>().material.name.Replace(" (Instance)", "").Equals("Right") || mouseOverObject != null && mouseOverObject.GetComponent<Renderer>().material.name.Replace(" (Instance)", "").Equals("Wrong"))
+                    {
+                        
+                        mouseOverObject.GetComponent<Renderer>().material = materialNormal;
+                    }
+                }
+                         
+                
             }
         }
+
+
+
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || Input.GetMouseButtonUp(0))
         {
             RaycastHit hitInfo = new RaycastHit();
@@ -66,10 +122,31 @@ public class LinesController : MonoBehaviour
                 }
                 else
                 {
-                    lineRend.SetPosition(1, new Vector3(hitInfo.collider.gameObject.transform.position.x, hitInfo.collider.gameObject.transform.position.y, 0f));
-                    drawingDone = true;
-                    Instantiate(Resources.Load("Prefabs/LineRenderer"));
-                    enabled = false;
+                    int startingNumber = int.Parse(startingObject.name);
+                    int mouseOverObjectNumber = int.Parse(hitInfo.collider.gameObject.name);
+
+                    if ((mouseOverObjectNumber == startingNumber + 1 && (mouseOverObjectNumber % numberOfColumns) != 1) || (mouseOverObjectNumber == startingNumber - 1 && (mouseOverObjectNumber % numberOfColumns) != 0) || mouseOverObjectNumber == startingNumber + numberOfColumns || mouseOverObjectNumber == startingNumber - numberOfColumns)
+                    {
+                        lineRend.SetPosition(1, new Vector3(hitInfo.collider.gameObject.transform.position.x, hitInfo.collider.gameObject.transform.position.y, 0f));
+                        hitInfo.collider.gameObject.GetComponent<Renderer>().material = materialNormal;
+
+                        if(startingNumber<mouseOverObjectNumber)
+                            gameControllerScript.addToList(startingNumber.ToString() + mouseOverObjectNumber.ToString());
+                        if(startingNumber>mouseOverObjectNumber)
+                            gameControllerScript.addToList(mouseOverObjectNumber.ToString() + startingNumber.ToString());
+                        
+                        drawingDone = true;
+                        gameControllerScript.getNumberOfLines(startingNumber,numberOfColumns,mouseOverObjectNumber);
+                        Instantiate(Resources.Load("Prefabs/LineRenderer"));
+                        enabled = false;
+                    }
+                    else
+                    {
+                        lineRend.SetPosition(0, new Vector3(0f, 0f, 0f));
+                        lineRend.SetPosition(1, new Vector3(0f, 0f, 0f));
+                        drawing = false;
+                        mouseOverObject.GetComponent<Renderer>().material = materialNormal;
+                    }
                 }
             }
             else
